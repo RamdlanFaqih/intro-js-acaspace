c64e30f38713ffebeb71e8cd9ee7aa9e2f065631
RamdlanFaqih <ramfaqih@gmail.com>
2025-07-12 11:34:50 +0700

    add data_types.js

---
+++ variable_and_data_types/2.data_types.js
@@ -0,0 +1,43 @@
+// --- Number ---
+let umur = 20;
+console.log(umur); // 20
+
+// --- String ---
+let nama = "Faqih";
+
+// how to declare
+const string1 = "Hello";
+const string2 = 'World';
+const string3 = `Hello World`
+console.log(nama); // "Faqih"
+
+// --- Boolean ---
+let isStudent = true;
+console.log(isStudent); // true
+
+// --- null ---
+let nilai = null;
+console.log(nilai); // null
+
+// --- undefined ---
+let alamat;
+console.log(alamat); // undefined
+
+// --- Array ---
+let hobi = ["ngoding", "makan", "gaming"];
+console.log(hobi); // ["ngoding", "makan", "gaming"]
+
+// --- Object ---
+let mahasiswa = {
+  nama: "Faqih",
+  umur: 20,
+  isStudent: true
+};
+console.log(mahasiswa); 
+// { nama: "Faqih", umur: 20, isStudent: true }
+
+// --- Function ---
+// Akan dibahas di next section
+// function sayHello() {
+//   console.log("Hello");
+// }
