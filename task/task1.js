06dcb29d9a0229d4591c8631c7d13fb4da04a2c7
RamdlanFaqih <ramfaqih@gmail.com>
2025-07-12 11:35:19 +0700

    add task

---
+++ task/task1.js
@@ -0,0 +1,10 @@
+// 1️⃣ Bikin struktur data untuk list handphone
+// - Harus mewakili semua tipe data yang sudah dijelaskan:
+//   string, number, boolean, null, undefined, array, object
+
+// 2️⃣ Cek nilai minimal KKM dari 5 data siswa
+// - KKM = 75
+// - Harus menghasilkan nilai true dan false sebagai hasil pengecekan
+
+// 3️⃣ Perbandingan login (object vs object)
+// - Cek apakah data login user (object) sama dengan data akun yang tersimpan (object)
