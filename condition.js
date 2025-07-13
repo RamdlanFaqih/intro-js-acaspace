810048939301c22d58f1a69b581ae91a6edf8cc8
RamdlanFaqih <ramfaqih@gmail.com>
2025-07-13 23:04:08 +0700

    add condition

---
+++ condition.js
@@ -0,0 +1,30 @@
+let umurNaekMotor = 17;
+let udin = 16;
+
+// if else statement
+if (udin > umurNaekMotor) {
+    console.log("Udin Boleh Bawa Motor")
+} else if (udin === umurNaekMotor) {
+    console.log("Umur pas banget, boleh bawa motor")
+} else {
+   console.log("Umur nya kurang ") 
+}
+
+
+let day = 4
+// 1 = senin, 2 = selasa, dst.
+
+// switch case statement
+switch (day) {
+    case 1:
+        console.log("senin")
+        break
+    case 2: 
+        console.log("selasa")
+        break
+    case 3:
+        console.log("rabu")
+        break;
+    default:
+        console.log("hari tidak terdefinisi")
+}
