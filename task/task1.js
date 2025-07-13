de6ea514aee802deda7b5b2eac35bb2941feffa6
RamdlanFaqih <ramfaqih@gmail.com>
2025-07-13 23:04:15 +0700

    pembahasan task1.js

--- task/task1.js
+++ task/task1.js
@@ -1,6 +1,8 @@
 // 1️⃣ Bikin struktur data untuk list handphone
 // - Harus mewakili semua tipe data yang sudah dijelaskan:
 //   string, number, boolean, null, undefined, array, object
+// 80 %
+// tambahin boolean, sama digabung jadi sebuah array of object
 
 // 2️⃣ Cek nilai minimal KKM dari 5 data siswa
 // - KKM = 75
@@ -8,3 +10,18 @@
 
 // 3️⃣ Perbandingan login (object vs object)
 // - Cek apakah data login user (object) sama dengan data akun yang tersimpan (object)
+
+
+// JSON.Stringify (cara compare 2 object pakai JSON.Stringify)
+const obj1 = {
+    nama: "faqih",
+    password: "Password"
+}
+
+const obj2 = {
+    nama: "faqih",
+    password: "Password"
+}
+
+console.log(obj1.nama === obj2.nama)
+console.log(obj1.password === obj2.password)
